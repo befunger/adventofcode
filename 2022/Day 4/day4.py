28534c6d05a7bf315ffd04fec32e23efe23df07e
befunger <alexander.d.willemsen@gmail.com>
2023-09-06 07:43:00 -0700

    removed "0 if, else 1" from summation

--- 2022/Day 4/day4.py
+++ 2022/Day 4/day4.py
@@ -4,14 +4,12 @@ with open("2022\Day 4\input.txt", "r") as file:
 nums = [[rng.split("-") for rng in line.strip("\n").split(",")] for line in pairs]
 
 # Part 1
-overlaps = sum([1 if 
-                int(line[0][0]) <= int(line[1][0]) and int(line[0][1]) >= int(line[1][1]) or 
-                int(line[0][0]) >= int(line[1][0]) and int(line[0][1]) <= int(line[1][1]) else 0 for line in nums])
+overlaps = sum([int(line[0][0]) <= int(line[1][0]) and int(line[0][1]) >= int(line[1][1]) or 
+                int(line[0][0]) >= int(line[1][0]) and int(line[0][1]) <= int(line[1][1]) for line in nums])
 print(overlaps)
 
 # Part 2
-overlaps = sum([1 if 
-                int(line[0][0]) in range(int(line[1][0]), int(line[1][1])+1) or 
+overlaps = sum([int(line[0][0]) in range(int(line[1][0]), int(line[1][1])+1) or 
                 int(line[0][1]) in range(int(line[1][0]), int(line[1][1])+1) or
-                int(line[1][0]) in range(int(line[0][0]), int(line[0][1])+1) else 0 for line in nums])
+                int(line[1][0]) in range(int(line[0][0]), int(line[0][1])+1) for line in nums])
 print(overlaps)
