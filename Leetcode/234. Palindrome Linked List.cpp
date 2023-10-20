c5d353d17c41f3639c5941d95565242c654d999c
befunger <alexander.d.willemsen@gmail.com>
2023-10-20 08:31:16 -0700

    O(1) space solution with in-place reversing

--- Leetcode/234. Palindrome Linked List.cpp
+++ Leetcode/234. Palindrome Linked List.cpp
@@ -15,6 +15,38 @@ public:
     bool isPalindrome(ListNode* head) {
         /*Check if linked list is palindromic (entries mirror across the middle)*/
 
+        /*O(n) time, O(1) space by in-place reversing second half of the list and comparing*/
+        ListNode *fast_ptr = head;
+        ListNode *slow_ptr = head;
+        bool move_slow = true;
+        // Find middle of list
+        while(fast_ptr != nullptr){
+            fast_ptr = fast_ptr->next;
+            if(move_slow){
+                slow_ptr = slow_ptr->next;
+            }
+            move_slow = !move_slow;
+        }
+        // Reverse second half in-place
+        ListNode *prev = nullptr;
+        ListNode *curr = slow_ptr;
+        ListNode *after;
+        while(curr != nullptr){
+            after = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = after;
+        }
+        ListNode *reverse_head = prev;
+
+        // Compare halves
+        while(reverse_head != nullptr){
+            if (reverse_head->val != head->val) return false;
+            head = head->next;
+            reverse_head = reverse_head->next;
+        }
+        return true;
+
         /*O(n) time, O(n) space using stack of first half values*/
         ListNode *fast_ptr = head;
         ListNode *slow_ptr = head;
