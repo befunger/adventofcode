c7ea4678ced7dbf2749ecde0ed19fe82113df7f7
befunger <alexander.d.willemsen@gmail.com>
2023-11-01 12:15:34 -0700

    moved reversing to separate function

--- Leetcode/92. Reverse Linked List II.cpp
+++ Leetcode/92. Reverse Linked List II.cpp
@@ -9,35 +9,42 @@ struct ListNode {
 class Solution {
 public:
     void reverseNextK(ListNode* &node, int K){
-        /*Given a node, reverses the proceeding K nodes in that list*/
+        /*Given a node, reverses the proceeding K nodes in that list.*/
+        
+        // If there are less than K nodes proceeding, do nothing
+        ListNode* temp = node;
+        for(int i=0; i < K; i++){
+            temp = temp->next;
+            if(!temp){return;}
+        }
+
         ListNode *prev = node;
         ListNode *curr = prev->next;
         ListNode *after;
-        for(int i = 0; i < K; i++){
+        for(int i = 1; i < K; i++){
             after = prev->next;
             prev->next = curr->next;
             curr->next = curr->next->next;
             prev->next->next = after;
         }
     }
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        /*Reverse part of linked list with indices [left, right]*/
-        if(left == right){return head;}
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        /*Reverse every group of size K in the list individually. Leftover nodes at end remain unchanged*/
+        if(k==1){return head;}
 
         // Create placeholder node before head
         ListNode *pre_head = new ListNode(0);
         pre_head->next = head;
 
-        // Iterate to node before first to swap (placeholder if left = 1)
-        ListNode* prev = pre_head;
-        for(int i = 1; i<left; i++){
-            prev = prev->next;
+        int i = 0;
+        ListNode *node = pre_head;
+        while(node){
+            if(i%k == 0){ // Flip at every k:th node
+                reverseNextK(node, k);
+            }
+            node = node->next;
+            i++;
         }
-
-        // Reverse next right-left nodes
-        reverseNextK(prev, right-left);
-
-        // Return first node in real list (might have changed if swap interval includes first)
         return pre_head->next;
     }
 };
