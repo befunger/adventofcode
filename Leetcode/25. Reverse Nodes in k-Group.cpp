b82e77b7c11f6283696db65a08b87c99156ba54f
befunger <alexander.d.willemsen@gmail.com>
2023-11-01 14:16:08 -0700

    reverse nodes in k-group

--- Leetcode/25. Reverse Nodes in k-Group.cpp
+++ Leetcode/25. Reverse Nodes in k-Group.cpp
@@ -20,15 +20,23 @@ public:
             prev->next->next = after;
         }
     }
-
     ListNode* reverseKGroup(ListNode* head, int k) {
-        /*Reverse every group of size K in the list individually. Any leftover nodes at end remain unchanged*/
+        /*Reverse every group of size K in the list individually. Leftover nodes at end remain unchanged*/
         if(k==1){return head;}
 
         // Create placeholder node before head
         ListNode *pre_head = new ListNode(0);
         pre_head->next = head;
 
+        int i = 0;
+        ListNode *node = pre_head;
+        while(node){
+            if(i%k == 0){ // Flip at every k:th node
+                reverseNextK(node, k);
+            }
+            node = node->next;
+            i++;
+        }
 
         return pre_head->next;
     }
