8c4be591bff805562cb8d80740edb8dae45937b3
befunger <alexander.d.willemsen@gmail.com>
2023-11-02 09:10:00 -0700

    fixed memory leak with new keyword

--- Leetcode/25. Reverse Nodes in k-Group.cpp
+++ Leetcode/25. Reverse Nodes in k-Group.cpp
@@ -3,7 +3,9 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
@@ -32,12 +34,15 @@ public:
         /*Reverse every group of size K in the list individually. Leftover nodes at end remain unchanged*/
         if(k==1){return head;}
 
-        // Create placeholder node before head
-        ListNode *pre_head = new ListNode(0);
-        pre_head->next = head;
+        // NOTE: This apparently causes a memory leak as new keyword stores variable on the heap (dynamic) and does not clean up
+        // ListNode *pre_head = new ListNode(0);
+        // pre_head->next = head;
+
+        // Create placeholder node before head (on heap)
+        ListNode pre_head(0, head);
 
         int i = 0;
-        ListNode *node = pre_head;
+        ListNode *node = &pre_head;
         while(node){
             if(i%k == 0){ // Flip at every k:th node
                 reverseNextK(node, k);
@@ -46,6 +51,6 @@ public:
             i++;
         }
 
-        return pre_head->next;
+        return pre_head.next;
     }
 };
