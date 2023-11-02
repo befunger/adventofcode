06e9e52e0dca1f5e8ceb71f550aa20eb7fa65287
befunger <alexander.d.willemsen@gmail.com>
2023-11-02 14:14:48 -0700

    zigzag binary tree level

---
+++ Leetcode/103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+//Definition for a binary tree node. 
+struct TreeNode {     
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        /*Same as 102 but every other level is reverse order*/
+        vector<vector<int>> level_order_traversal;
+        if(!root){return level_order_traversal;}
+
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        vector<int> level; 
+        int level_size = 1; // Number of elements left in the current level
+        bool reverse_level = false; // Toggle each level to reverse/unreverse
+
+        // Keep going until no elements in the queue
+        while(!nodes.empty()){
+            // Pop first element and add to level
+            TreeNode* node = nodes.front();
+            nodes.pop();
+            level.push_back(node->val);
+
+            // Add potential children to queue
+            if(node->left){
+                nodes.push(node->left);
+            }
+            if(node->right){
+                nodes.push(node->right);
+            }
+
+            level_size--;
+            if(level_size == 0){ // When finished with one level, start next level and reset size
+                if(reverse_level){
+                    reverse(level.begin(),level.end()); 
+                }
+                reverse_level = !reverse_level; // Swap each layer if we reverse or not
+
+                level_order_traversal.push_back(level);
+                level_size = nodes.size(); // All remaining are next level
+                level = vector<int>();
+            }
+        }
+
+        return level_order_traversal;
+    }
+};
