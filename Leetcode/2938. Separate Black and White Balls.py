2f58e4e9520fb51ad8dadb7603a71f78b23cb3be
befunger <alexander.d.willemsen@gmail.com>
2023-11-21 12:12:16 -0800

    comments added

--- Leetcode/2938. Separate Black and White Balls.py
+++ Leetcode/2938. Separate Black and White Balls.py
@@ -1,8 +1,9 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
+        # To sort the array, each 0 (white ball) must be swapped with each black ball (1) on its left
         ones_passed, swaps = 0, 0
         for bit in s:
-            if bit == "1": 
+            if bit == "1":
                 ones_passed += 1
             else:
                 swaps += ones_passed
