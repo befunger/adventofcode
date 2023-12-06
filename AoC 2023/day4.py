6b1417a810449f21e342e9118f8be7f29e7d9c54
befunger <alexander.d.willemsen@gmail.com>
2023-12-06 08:13:35 -0800

    simplify day4

--- AoC 2023/day4.py
+++ AoC 2023/day4.py
@@ -6,10 +6,11 @@ with open("AoC 2023\inputs\day4.txt", "r") as file:
 points = 0
 for line in inp:
     # Set of winning numbers (remove empty strings)
-    win_set = set(x for x in line[0].split(" ") if x != '' and x != ' ')
+    win_set = set(line[0].split())
     # Amount of numbers on cards that are also in the win set
-    num_matched = len([y for y in line[1].split(" ") if y in win_set])
-    points += 0 if num_matched == 0 else 2**(num_matched-1)
+    num_matched = len([y for y in line[1].split() if y in win_set])
+    if num_matched == 0:
+        points += 2**(num_matched-1)
 print("Part 1:", points)
 
 '''Part 2'''
@@ -17,9 +18,9 @@ print("Part 1:", points)
 cards = [1]*len(inp)
 for i, line in enumerate(inp):
     # Set of winning numbers (remove empty strings)
-    win_set = set(x for x in line[0].split(" ") if x != '' and x != ' ')
+    win_set = set(line[0].split())
     # Amount of numbers on cards that are also in the win set
-    num_matched = len([y for y in line[1].split(" ") if y in win_set])
+    num_matched = len([y for y in line[1].split() if y in win_set])
     # Increase number of the next [num_matched] scratch cards by 1 for each copy of this card
     # Scratch card X matches 3 numbers and has 5 copies => increase cards X+1, X+2, and X+3 by 5
     while num_matched:
