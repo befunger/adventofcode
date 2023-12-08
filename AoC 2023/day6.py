d32657c5d62cc432559c39b12c33d7d2985f122e
befunger <alexander.d.willemsen@gmail.com>
2023-12-08 05:21:15 -0800

    comments clarified

--- AoC 2023/day6.py
+++ AoC 2023/day6.py
@@ -1,22 +1,26 @@
 from math import sqrt
 with open("AoC 2023\inputs\day6.txt", "r") as file:
-    # Remove linebreaks, remove 'Blabla:' prefix, split on whitespace, and cast to int
+    # Remove linebreaks, remove 'Blabla:' prefix, and split on whitespace
     inp = [line.strip("\n").split(": ")[1].split() for line in file.readlines()]
 
+# Given a total time t_max, holding down the button for time t our travel distance D:
+# D = t * (t_max-t) = t*t_max - t^2
+# We wish to find t that give D(t) > D_max. Start by finding intersections D(t) = D_max
+# t*t_max - t^2 = D_max =>
+# -t^2 + t*t_max - D_max = 0 => {Isolate t}
+# t = t_max/2 +- sqrt((t_max/2)^2 - D_max)
+# The number of winning choices is equal to the number of integers within the interval
+# (lowest, highest), make sure not to include actual intersections if they're integers
 
 def numberOfWinningChoices(times, dists):
-    # Given a total time t_max, holding down the button for time t our travel distance D is
-    # D = t * (t_max-t) = t*t_max - t^2
-    # We wish to find t that give D(t) > D_max
-    # -t^2 + t*t_max - D_max = 0 => {p = -t_max, q = D_max} => t = t_max/2 +- sqrt((t_max/2)^2 - D_max)
     output = 1
     for t_max, D_max in zip(times, dists):
-        # Find both intersection points where D(t) = D_max
+        # Find intersection points where D(t) = D_max
         lowest_win = t_max/2 - sqrt(t_max*t_max/4 - D_max)
         highest_win = t_max/2 + sqrt(t_max*t_max/4 - D_max)
         print(f"Intersect at t={lowest_win} and t={highest_win}")
         # Cast to int to get range of discrete values that solve
-        # I perturb both roots slightly for the edge case where a root is exactly an integer value
+        # I perturb both roots slightly for the edge case where a root is an integer
         output *= (int(highest_win-1e-10) - int(lowest_win+1e-10))
     return output
 
